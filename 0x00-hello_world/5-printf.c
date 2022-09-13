b8d029af5956d1613b584739bd338b177a5f0d70
LynetMwikali <mwikalilin@gmail.com>
2022-09-13 05:13:46 -0700

    added 5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main-entry point for the program
  *
- * Return:Always 0(Success/correct)
+ *Return: Always 0(success/correct)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,|n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
