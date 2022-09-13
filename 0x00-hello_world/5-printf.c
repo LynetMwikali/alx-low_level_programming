6397119a7ceeceddf8ba8268ff655e3207eeba9b
LynetMwikali <mwikalilin@gmail.com>
2022-09-13 02:26:49 -0700

    added 5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
  *main-a program to print out a statement
+*
 *Return:Always 0 if no errors
  */
 int main(void)
 {
-printf("with proper grammar,but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
