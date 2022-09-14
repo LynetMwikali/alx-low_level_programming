f7426a6d5b532e169d50cc059d810c9e904c5468
LynetMwikali <mwikalilin@gmail.com>
2022-09-14 05:28:28 -0700

    added main.h file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,14 @@
-#include<stdio.h>
+#include<main.h>
 /**
  * main-a program that prints _putchar,followed by a new line
  * Return:Always 0 if correct
  */
+int _putchar()
+{
+	_putchar("_putchar\n");
+}
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar;
 	return (0);
 }
