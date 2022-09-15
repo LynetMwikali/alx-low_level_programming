1f644797249a6a29250dfec9e00ae46096c5adad
LynetMwikali <mwikalilin@gmail.com>
2022-09-15 13:35:13 -0700

    added 1-isdigit.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*main-a program that multiplies two integers
+*@a:digit to be multiplied
+*@b:digit to be multiplied
+*Return: Always 0.
+**/
+int mul(int a, int b);
+{
+
+int result;
+
+result = a * b;
+
+return result;
+}
+
+
+
