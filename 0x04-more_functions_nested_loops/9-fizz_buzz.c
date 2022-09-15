fd820752bd730b65425f339327ab20e30e6c3d5f
LynetMwikali <mwikalilin@gmail.com>
2022-09-15 15:26:46 -0700

    added Fizzbuzz file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,32 +1,33 @@
-#include "main.h"
 #include <stdio.h>
+
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+
 int main(void)
 {
-	int a;
+	int i;
 
-	for (a = 1; a <= 100; a++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (a % 3 == 0 && a % 5 == 0)
-		{
+		if (((i % 3) || (i % 5)) == 0)
 			printf("FizzBuzz");
-		}
-		else if (a % 3 == 0)
-		{
+
+		else if ((i % 3) == 0)
 			printf("Fizz");
-		}
-		else if (a % 5 == 0)
-		{
+
+		else if ((i % 5) == 0)
 			printf("Buzz");
-		}
+
 		else
-			printf("%d", a);
-		if (a != 100)
+			printf("%d", i);
+		if (i != 100)
 			printf(" ");
 	}
+
 	printf("\n");
+
 	return (0);
+}
