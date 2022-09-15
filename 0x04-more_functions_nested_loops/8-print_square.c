0fe0ca0fbf4a2513af94a81c9e837736e21d98df
LynetMwikali <mwikalilin@gmail.com>
2022-09-15 15:34:27 -0700

    added fizzbuzz

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,21 +1,26 @@
 #include "main.h"
 /**
- * print_square - function to print squares
- * @size : integer value
- * Return: Always 0.
+ * print_square - print a square
+ * @size: size of square
+ * Description: Can only use _putchar to print. Use '#' to print square.
  */
 void print_square(int size)
 {
-	int a, b;
+	int x, y;
 
-	if (size > 0)
-	{
-		for (a = 0; a < size; a++)
-		{
-			for (b = 0; b < size; b++)
-				_putchar('#');
-			_putchar('\n');
-		}
-	}
-	else
+	y = 0;
+
+	if (size < 1)
 		_putchar('\n');
+
+	while (y < size)
+	{
+		x = 0;
+		while (x < size)
+		{
+			_putchar('#');
+			x++;
+		}
+		_putchar('\n');
+		y++;
+	}
