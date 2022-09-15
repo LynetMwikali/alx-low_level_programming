a3a011d6260a0019544624d18070fe002b041bbc
LynetMwikali <mwikalilin@gmail.com>
2022-09-15 02:10:50 -0700

    added 0-main.c file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**main-tests if a number is 0
+ * Return:Always 0 if success
+ */
+int main(void)
+{
+	int i;
+	
+	i = 98;
+	;
+	while(i=0)
+	{
+		printf("i is 0\n");
+	}
+	return (0);
+}
+
