75dc0a35b094fca8acd97774053e178b60d57eb6
LynetMwikali <mwikalilin@gmail.com>
2022-09-15 02:25:30 -0700

    added 0-main.c file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
-/**main-tests if a number is 0
- * Return:Always 0 if success
- */
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	int i;
-	
-	i = 98;
-	;
-	while(i=0)
-	{
-		printf("i is 0\n");
-	}
-	return (0);
-}
+int i;
 
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
