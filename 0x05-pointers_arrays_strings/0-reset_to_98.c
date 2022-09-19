d7888e32c6ea94a48316633abd4c6dd14a674fbc
LynetMwikali <mwikalilin@gmail.com>
2022-09-19 08:39:18 -0700

    added 0-reset_to_98.c file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main-function that updates the value it points to 98(int)
+ * void reset_to_98: updtaes the value to 98
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n = 98;
+	int *p = &n;
+
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+C projects that test the following concepts;arrays,pointers,strings and memory layout
